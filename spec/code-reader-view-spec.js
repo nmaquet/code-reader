6ecd8c3c9d0ba97bfde875110674ab7e8e95b680
Nicolas Maquet <nicolas@movio.co>
2018-05-11 20:12:28 +1200

    initial import

---
+++ spec/code-reader-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodeReaderView from '../lib/code-reader-view';
+
+describe('CodeReaderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
